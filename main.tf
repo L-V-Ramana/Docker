959b2fa7676c5cbac153f85aef0d7e0e0fae9d30
L V Ramana <lellalvramana@gmail.com>
2025-08-02 12:10:33 +0530

    docker

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "main"{
     ami    =  local.ami
-    instance_type = "t3.medium"
+    instance_type = "t3.micro"
     vpc_security_group_ids = [aws_security_group.allow_all_docker.id]
 
      root_block_device {
